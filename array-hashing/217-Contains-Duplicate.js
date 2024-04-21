bfd61862381b11f1cf84a0fbbedb5ef36d43e1e4
Devika Mujgule <mujgule.d@northeastern.edu>
2024-04-21 17:12:06 -0400

    217 all approaches

--- array-hashing/217-Contains-Duplicate.js
+++ array-hashing/217-Contains-Duplicate.js
@@ -1,13 +1,39 @@
 `use strict`
-
+//OPTIMIZED
+// DS - Array, Set
 var containsDuplicate = function (nums) {
   const numsSetSize = new Set(nums).size;
   // nums arry len > unique set length
   return nums.length > numsSetSize ? true : false;
+ 
 };
-const test1 = [1, 1, 1, 1, 2, 2, 2]
+const test1 = [1, 2, 3, 1]
 containsDuplicate(test1);
-
 console.log(`result`, containsDuplicate(test1));
 
+ //APPROACH 2 //BRUTE - O(n^2)
 
+//  for (let i = 0; i < nums.length; i++) {
+//   console.log(`nums i`, nums[i]);
+
+//   for (let j = i + 1; j < nums.length; j++) {
+//     console.log(`nums j`, nums[j]);
+
+//     if (nums[i] === nums[j]) {
+//       // console.log(`false`);
+//       return true
+//     }
+//   }
+// }
+// return false;
+
+// APPROACH 3
+//NOT THE BEST BUT WORKS 
+// nums.sort();
+//  for (let i = 0; i < nums.length; i++) {
+//       if (nums[i] === nums[i +1]) {
+//         return true
+//       };
+//     }
+
+//   return false
